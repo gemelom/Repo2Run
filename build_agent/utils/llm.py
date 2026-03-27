c64923ad0474dd576f1a2f6b06ed2ebd3becca9d
gemelom <3486609936@qq.com>
2026-03-27 19:26:17 +0800

    fix typo

--- build_agent/utils/llm.py
+++ build_agent/utils/llm.py
@@ -1,23 +1,23 @@
-# Copyright (2025) Bytedance Ltd. and/or its affiliates 
+# Copyright (2025) Bytedance Ltd. and/or its affiliates
 
-# Licensed under the Apache License, Version 2.0 (the "License"); 
-# you may not use this file except in compliance with the License. 
-# You may obtain a copy of the License at 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 
 #     https://www.apache.org/licenses/LICENSE-2.0
 
-# Unless required by applicable law or agreed to in writing, software 
-# distributed under the License is distributed on an "AS IS" BASIS, 
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-# See the License for the specific language governing permissions and 
-# limitations under the License. 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 from openai import OpenAI
 import time
 import os
 
-DEEPSEEK_API_KEY = os.getenv("DEEPEEK_API_KEY")
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY", "")
 
 model_configs = {
     "deepseek-chat": {
@@ -26,11 +26,12 @@ model_configs = {
     },
 }
 client = OpenAI(
-    base_url=model_configs["deepseek-chat"]["base_url"], api_key=model_configs["deepseek-chat"]["key"]
+    base_url=model_configs["deepseek-chat"]["base_url"],
+    api_key=model_configs["deepseek-chat"]["key"],
 )
 
 
-def get_llm_response(model: str, messages, temperature = 0.0, n = 1, max_tokens = 1024):
+def get_llm_response(model: str, messages, temperature=0.0, n=1, max_tokens=1024):
     max_retry = 5
     count = 0
     while count < max_retry:
@@ -40,7 +41,7 @@ def get_llm_response(model: str, messages, temperature = 0.0, n = 1, max_tokens 
                 messages=messages,
                 temperature=temperature,
                 n=n,
-                max_tokens=max_tokens
+                max_tokens=max_tokens,
             )
             return response.choices[0].message.content, response.usage
         except Exception as e:
